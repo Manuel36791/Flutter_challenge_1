fc81219875ec9b2c757ee13e303d65c3cc3c7170
Manuel36791 <manuelvictor36791@gmail.com>
2021-09-08 18:07:36 +0200

    login from mano

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_challenge_1/modules/login_screen/login_screen.dart';
 
+
+//hello world you cant meet the world without know how to say hello
+/// my name is Manuel and I'm a junior flutter developer
 void main() {
   runApp(MyApp());
 }
